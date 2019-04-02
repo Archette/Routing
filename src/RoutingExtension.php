82f83f3ce04e071619776f6b278219814f6ae82f
Rick Strafy <rixafy@gmail.com>
2019-04-02 23:14:27 +0200

    Add services to DI

--- src/RoutingExtension.php
+++ src/RoutingExtension.php
@@ -4,7 +4,41 @@ declare(strict_types=1);
 
 namespace Archette\Routing;
 
-class RoutingExtension
-{
+use Doctrine\Common\Persistence\Mapping\Driver\AnnotationDriver;
+use Nette\DI\CompilerExtension;
+use Rixafy\Routing\Route\Group\RouteGroupFacade;
+use Rixafy\Routing\Route\Group\RouteGroupFactory;
+use Rixafy\Routing\Route\Group\RouteGroupRepository;
+use Rixafy\Routing\Route\RouteFacade;
+use Rixafy\Routing\Route\RouteFactory;
+use Rixafy\Routing\Route\RouteRepository;
 
+class RoutingExtension extends CompilerExtension
+{
+    public function beforeCompile()
+    {
+        $this->getContainerBuilder()->getDefinitionByType(AnnotationDriver::class)
+            ->addSetup('addPaths', [['vendor/rixafy/routing']]);
+    }
+
+    public function loadConfiguration()
+    {
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeFacade'))
+            ->setFactory(RouteFacade::class);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeRepository'))
+            ->setFactory(RouteRepository::class);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeFactory'))
+            ->setFactory(RouteFactory::class);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeGroupFacade'))
+            ->setFactory(RouteGroupFacade::class);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeGroupRepository'))
+            ->setFactory(RouteGroupRepository::class);
+
+        $this->getContainerBuilder()->addDefinition($this->prefix('routeGroupFactory'))
+            ->setFactory(RouteGroupFactory::class);
+    }
 }
